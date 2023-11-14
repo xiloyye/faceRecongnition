cd2bfffe0f4a8ce708ec046d43252d30e4a0c06a
xiloyye <115136750+xiloyye@users.noreply.github.com>
2023-11-14 16:00:27 +0800

    Update siameseNetwork.py

--- siameseNetwork.py
+++ siameseNetwork.py
@@ -1,85 +1,81 @@
-class SiameseNetworkDataset(Dataset):
+class Attention(nn.Module):
+    def __init__(self, in_channels):
+        super(Attention, self).__init__()
+        #输入及输出都为3通道，不改变原始图片通道数
+        self.conv = nn.Conv2d(in_channels, 3, kernel_size=1, bias=False)
+    def forward(self, x):
+        a = F.relu(self.conv(x))
+        a = F.softmax(a.view(a.size(0), -1), dim=1).view_as(a)
+        x = x * a
+        return x
     
-    def __init__(self,imageFolderDataset,transform=None,should_invert=True):
-        self.imageFolderDataset = imageFolderDataset    
-        self.transform = transform
-        self.should_invert = should_invert
-        
-    def __getitem__(self,index):
-        img0_tuple = random.choice(self.imageFolderDataset.imgs)
-        #we need to make sure approx 50% of images are in the same class
-        should_get_same_class = random.randint(0,1) 
-        if should_get_same_class:
-            while True:
-                #keep looping till the same class image is found
-                img1_tuple = random.choice(self.imageFolderDataset.imgs) 
-                if img0_tuple[1]==img1_tuple[1]:
-                    break
-        else:
-            while True:
-                #keep looping till a different class image is found
-                
-                img1_tuple = random.choice(self.imageFolderDataset.imgs) 
-                if img0_tuple[1] !=img1_tuple[1]:
-                    break
+class SiameseNetwork(nn.Module):
+    def __init__(self):
+        super(SiameseNetwork, self).__init__()
+        self.cnn1 = nn.Sequential(
+            nn.ReflectionPad2d(1),
+            nn.Conv2d(1, 4, kernel_size=3),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(4),
+            
+            nn.ReflectionPad2d(1),
+            nn.Conv2d(4, 8, kernel_size=3),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(8),
 
-        img0 = Image.open(img0_tuple[0])
-        img1 = Image.open(img1_tuple[0])
-        img0 = img0.convert("L")
-        img1 = img1.convert("L")
-        
-        if self.should_invert:
-            img0 = PIL.ImageOps.invert(img0)
-            img1 = PIL.ImageOps.invert(img1)
+            nn.ReflectionPad2d(1),
+            nn.Conv2d(8, 8, kernel_size=3),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(8),
+        )
 
-        if self.transform is not None:
-            img0 = self.transform(img0)
-            img1 = self.transform(img1)
-        
-        return img0, img1 , torch.from_numpy(np.array([int(img1_tuple[1]!=img0_tuple[1])],dtype=np.float32))
-    
-    def __len__(self):
-        return len(self.imageFolderDataset.imgs)
-    
-class SiaDataset_rgb(Dataset):
-    
-    def __init__(self,imageFolderDataset,transform=None,should_invert=True):
-        self.imageFolderDataset = imageFolderDataset    
-        self.transform = transform
-        self.should_invert = should_invert
-        
-    def __getitem__(self,index):
-        img0_tuple = random.choice(self.imageFolderDataset.imgs)
-        #we need to make sure approx 50% of images are in the same class
-        should_get_same_class = random.randint(0,1) 
-        if should_get_same_class:
-            while True:
-                #keep looping till the same class image is found
-                img1_tuple = random.choice(self.imageFolderDataset.imgs) 
-                if img0_tuple[1]==img1_tuple[1]:
-                    break
-        else:
-            while True:
-                #keep looping till a different class image is found
-                
-                img1_tuple = random.choice(self.imageFolderDataset.imgs) 
-                if img0_tuple[1] !=img1_tuple[1]:
-                    break
+        self.fc1 = nn.Sequential(
+            nn.Linear(8*100*100, 500),
+            nn.ReLU(inplace=True),
+            nn.Linear(500, 500),
+            nn.ReLU(inplace=True),
+            nn.Linear(500, 5))
+    def forward_once(self, x):
+        output = self.cnn1(x)
+        output = output.view(output.size()[0], -1)
+        output = self.fc1(output)
+        return output
 
-        img0 = Image.open(img0_tuple[0])
-        img1 = Image.open(img1_tuple[0])
-        img0 = img0.convert("RGB")
-        img1 = img1.convert("RGB")
-        
-        if self.should_invert:
-            img0 = PIL.ImageOps.invert(img0)
-            img1 = PIL.ImageOps.invert(img1)
-
-        if self.transform is not None:
-            img0 = self.transform(img0)
-            img1 = self.transform(img1)
-        
-        return img0, img1 , torch.from_numpy(np.array([int(img1_tuple[1]!=img0_tuple[1])],dtype=np.float32))
+    def forward(self, input1, input2):
+        output1 = self.forward_once(input1)
+        output2 = self.forward_once(input2)
+        return output1, output2
     
-    def __len__(self):
-        return len(self.imageFolderDataset.imgs)
+import torch.nn as nn
+import torchvision
+import torch.nn.functional as F
+
+class sia_net(nn.Module):
+    def __init__(self , model):
+        super(sia_net, self).__init__()
+        #取掉model的后两层
+        self.fc1 = nn.Sequential(
+                nn.Sequential(*list(model.children())[:-2]),
+                nn.AdaptiveAvgPool2d(1))
+
+        self.fc1_0 = nn.Sequential(
+                nn.Linear(2048, 1024),
+                nn.Linear(1024, 512))
+        self.attention1 = Attention(1)
+
+    def forward_once(self, x):
+        x = self.attention1(x)
+        x = self.fc1(x)
+        x = x.view(x.size()[0], -1) 
+        feature = self.fc1_0(x)     #feature
+        return feature
+    
+    def forward(self, input_l, input_r):
+        feature_l = self.forward_once(input_l)
+        feature_r = self.forward_once(input_r)
+        return feature_l, feature_r
+
+def load_resnet50():
+    resnet = torchvision.models.resnet50()
+    model = sia_net(resnet)
+    return model
